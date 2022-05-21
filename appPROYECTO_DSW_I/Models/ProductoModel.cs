01583a7b0ff8070d4f9f01e4d45f72393da4dbc6
Jezrel <Jezrel_talledo2100@hotmail.com>
2022-05-21 11:53:24 -0500

    commit de productoModel

---
+++ appPROYECTO_DSW_I/Models/ProductoModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace appPROYECTO_DSW_I.Models
+{
+    public class ProductoModel
+    {
+        public string nombreProd { get; set; }
+    }
+}
