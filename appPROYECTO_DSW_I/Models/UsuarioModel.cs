3c5a8aad32e0721a8cb6a05e07d1b9e2596924aa
JosueNanez <josue.esforzador@gmail.com>
2022-05-21 11:42:57 -0500

    Pruba Comit 01

---
+++ appPROYECTO_DSW_I/Models/UsuarioModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace appPROYECTO_DSW_I.Models
+{
+    public class UsuarioModel
+    {
+        public string nombre { get; set; }
+    }
+}
