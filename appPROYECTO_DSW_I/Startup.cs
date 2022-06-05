914fad8d5ad3c7f51b6b49ab96bcad82927e8ce1
jezrel <Jezrel_talledo2100@hotmail.com>
2022-06-04 20:50:49 -0500

    commit - arreglo vista index

--- appPROYECTO_DSW_I/Startup.cs
+++ appPROYECTO_DSW_I/Startup.cs
@@ -66,7 +66,7 @@ namespace appPROYECTO_DSW_I
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Sesion}/{action=LoginCookie}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
         });
         }
     }
